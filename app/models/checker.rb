1a1fc3b222daf59b7bb68cf8ada5cce5fb7e16ea
Miha Rekar <miha.rekar@gmail.com>
2013-11-11 11:30:34 +0100

    refactored regex definitions into method

--- app/models/checker.rb
+++ app/models/checker.rb
@@ -2,14 +2,6 @@ class Checker
   attr_reader :corrected, :original, :highlighted
 
   CHECKS = [:space, :ellipsis, :z, :s, :h, :k, :capitals]
-  REPLACEMENTS = { space: ' ', ellipsis: '…', z: 's', s: 'z', h: 'k', k: 'h' }
-  SPACE_REGEX = /  +/
-  ELLIPSIS_REGEX = /\.{2,}/
-  Z_REGEX = /(?<=\s)z(?=\s[cčfhkpsšt])|^z(?=\s[cčfhkpsšt])/i
-  S_REGEX = /(?<=\s)s(?=\s[^cčfhkpsšt])|^s(?=\s[^cčfhkpsšt])/i
-  K_REGEX = /(?<=\s)k(?=\s[kg])|^k(?=\s[kg])/i
-  H_REGEX = /(?<=\s)h(?=\s[^kg])|^h(?=\s[^kg])/i
-  CAPITALS_REGEX = /(?<=[.?!] )([a-zčšž])|^([a-zčšž])/
 
   def initialize text
     @original = text.strip
@@ -27,7 +19,7 @@ class Checker
 
   CHECKS.each do |check|
     define_method "#{check}_count" do
-      @original.scan(regex_from_symbol(check)).size
+      @original.scan(regexes[check]).size
     end
 
     define_method "#{check}_percent" do
@@ -36,15 +28,23 @@ class Checker
   end
 
   private
-  def regex_from_symbol symbol
-    self.class.const_get("#{symbol.upcase}_REGEX")
+  def regexes
+    {
+      space: /  +/,
+      ellipsis: /\.{2,}/,
+      z: /(?<=\s)z(?=\s[cčfhkpsšt])|^z(?=\s[cčfhkpsšt])/i,
+      s: /(?<=\s)s(?=\s[^cčfhkpsšt])|^s(?=\s[^cčfhkpsšt])/i,
+      k: /(?<=\s)k(?=\s[kg])|^k(?=\s[kg])/i,
+      h: /(?<=\s)h(?=\s[^kg])|^h(?=\s[^kg])/i,
+      capitals: /(?<=[.?!] )([a-zčšž])|^([a-zčšž])/
+    }
   end
 
   def correct_text before = '', after = ''
     corrected = @original
-    REPLACEMENTS.each do |key, value|
-      corrected = corrected.gsub(regex_from_symbol(key), "#{before}#{value}#{after}")
+    { space: ' ', ellipsis: '…', z: 's', s: 'z', h: 'k', k: 'h' }.each do |key, value|
+      corrected = corrected.gsub(regexes[key], "#{before}#{value}#{after}")
     end
-    corrected.gsub(CAPITALS_REGEX){ |s| "#{before}#{s.upcase}#{after}" }
+    corrected.gsub(regexes[:capitals]){ |s| "#{before}#{s.upcase}#{after}" }
   end
 end
